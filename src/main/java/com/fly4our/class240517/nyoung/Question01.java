15754fac8c39a7ea435269643747ed957af3444b
kosm0ssy <xzerokim@gmail.com>
2024-05-17 15:19:42 +0900

    update

---
+++ src/main/java/com/fly4our/class240517/nyoung/Question01.java
@@ -0,0 +1,12 @@
+package main.java.com.fly4our.class240517.nyoung;
+
+public class Question01 {
+
+//    1. String[] names = new String[10] <br/>
+//            배열의 마지막 셀에 "Hello"가 반환되는 방법은?
+//
+//            -[ ] A. names[0] = "Hello";
+//-[ ] B. names[10] = "Hello";
+//-[ ] C. names[9] = "Hello";
+//-[ ] D. String[ names.length-1 ] = "Hello";
+}
---
+++ src/main/java/com/fly4our/class240517/nyoung/Question02.java
@@ -0,0 +1,21 @@
+package main.java.com.fly4our.class240517.nyoung;
+
+public class Question02 {
+//
+//    2. String[] names = new String[10];
+//<br/> null 참조를 건너뛰고 모든 String을 출력하는 방법은?
+//
+//            -[ ] A.
+//for (int j = 0; names[j] != null; j++) <br/>
+//            System.out.println(names[j]);
+//-[ ] B.
+//for (int j = 0; j < names.length; j++) <br/>
+//            System.out.println(names[j]);
+//-[ ] C.
+//for (int j = 0; j < names.length; j++) <br/>
+//            System.out.println(names[j]);
+//-[ ] D.
+//for (int j = 0; j < names.length; j++) <br/>
+//            if(names[j] != null)
+//            System.out.println(names[j]);
+}

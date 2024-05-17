2187636365cc926210e4a67efb295a241f758cdd
kosm0ssy <xzerokim@gmail.com>
2024-05-17 13:30:53 +0900

    file upload

---
+++ src/main/java/com/fly4our/class240516/nyoung/Application.java
@@ -0,0 +1,24 @@
+package main.java.com.fly4our.class240516.nyoung;
+
+import java.util.Scanner;
+
+public class Application /*스캐너로 로그인, 회원가입, 프로그램 종료*/ {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=====회원 정보 프로그램=====");
+        System.out.println("1. 로그인");
+        System.out.println("2. 회원가입");
+        System.out.println("9. 프로그램 종료");
+
+        int no = sc.nextInt()
+
+        switch (sc.nextInt()) {
+            case 1 :
+        }
+
+    }
+
+}
---
+++ src/main/java/com/fly4our/class240516/nyoung/User.java
@@ -0,0 +1,4 @@
+package main.java.com.fly4our.class240516.nyoung;
+
+public class User /*id, pwd, name을 받을 DTO*/ {
+}
